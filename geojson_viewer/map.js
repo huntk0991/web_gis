45eee51011b50f3e7f4309bb7f65b3e61adc3680
hunt0991 <huntk0991@gmail.com>
2026-05-16 07:14:05 +0530

    Loads geojson static

--- geojson_viewer/map.js
+++ geojson_viewer/map.js
@@ -1,6 +1,14 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([71.505, 28.09], 13);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+fetch('sample.geojson')
+    .then(response => (response.json()))
+    .then(data=>{
+        const geojson_layer=L.geoJSON(data).addTo(map)
+        map.fitBounds(geojson_layer.getBounds())
+    });
+
