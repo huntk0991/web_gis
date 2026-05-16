2d1c3b7cb23867a5f41095a7d0ee487249b5f495
hunt0991 <huntk0991@gmail.com>
2026-05-16 06:47:28 +0530

    Loaded a leaflet map

--- geojson_viewer/map.js
+++ geojson_viewer/map.js
@@ -1,47 +1,6 @@
-// Create map
-const map = L.map('map').setView([20, 78], 5);
+var map = L.map('map').setView([51.505, -0.09], 13);
 
-// Add OpenStreetMap basemap
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; OpenStreetMap contributors'
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-
-// Load GeoJSON
-fetch('sample.geojson')
-    .then(response => response.json())
-    .then(data => {
-
-        console.log("GeoJSON loaded:", data);
-
-        // Add GeoJSON layer
-        const geojsonLayer = L.geoJSON(data, {
-
-            style: {
-                color: 'blue',
-                weight: 2
-            },
-
-            onEachFeature: function(feature, layer) {
-
-                // Popup with attributes
-                if (feature.properties) {
-
-                    let popupContent = "";
-
-                    for (let key in feature.properties) {
-                        popupContent += `<b>${key}</b>: ${feature.properties[key]}<br>`;
-                    }
-
-                    layer.bindPopup(popupContent);
-                }
-            }
-
-        }).addTo(map);
-
-        // Zoom to GeoJSON
-        map.fitBounds(geojsonLayer.getBounds());
-
-    })
-    .catch(error => {
-        console.error("Error loading GeoJSON:", error);
-    });
